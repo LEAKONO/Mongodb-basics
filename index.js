b29b078062f332429d3a486d70234ac3e4663873
LEAKONO <leakonoemmanuel3@gmail.com>
2025-04-26 20:48:14 +0300

    Connect server to MongoDB database

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require('express');
-const mongoose = require('mongoose');
-
 const app = express();
-app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/myMongoProject', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected!'))
-.catch((err) => console.error('MongoDB connection error:', err));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/myapp')
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
