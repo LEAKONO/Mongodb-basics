b7eebfeaac76d7d58e98f908edbeb7813d2b4d86
LEAKONO <leakonoemmanuel3@gmail.com>
2025-04-26 20:55:31 +0300

    Add POST route to create new users

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+// POST route to create a new user
+app.post('/users', async (req, res) => {
+    try {
+      const { name, email } = req.body;
+      const newUser = new User({ name, email });
+      await newUser.save();
+      res.status(201).json(newUser);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
 
-const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1:27017/myapp')
   .then(() => console.log('MongoDB connected'))
