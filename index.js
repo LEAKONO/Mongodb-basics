a43b75cb1e0ad018d9dc42b8d464d2a9a4d2c098
LEAKONO <leakonoemmanuel3@gmail.com>
2025-04-26 20:56:50 +0300

    Add GET route to fetch all users

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const mongoose = require('mongoose');
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-// POST route to create a new user
+app.get('/users', async (req, res) => {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
 app.post('/users', async (req, res) => {
     try {
       const { name, email } = req.body;
